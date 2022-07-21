a232144c78fc90c0e06cc669a5a0eb9c6ec6d814
mamunurrashid1010 <96053862+mamunurrashid1010@users.noreply.github.com>
2022-07-21 10:31:14 +0600

    Re-declaring a Variable Using let, example

---
+++ variable.js
@@ -0,0 +1,13 @@
+<!--
+    ES6
+    let variable example
+-->
+
+let x=10;
+document.getElementById('demo').innerHTML="x value is:"+x;
+
+{
+    let x=20;
+    document.getElementById('demo').innerHTML="x value is:"+x;
+    //here output is 20
+}
