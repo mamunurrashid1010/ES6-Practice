021b3093f6770ed0f8245887af22aec3f22bf6b2
mamunurrashid1010 <96053862+mamunurrashid1010@users.noreply.github.com>
2022-07-24 10:02:35 +0600

    arrow function using a program, subtraction & multiplication example added

--- arrowFunction.js
+++ arrowFunction.js
@@ -8,6 +8,20 @@ const sum=(x,y)=>{
     return x+y;
 }
 
+var sub=(x,y)=>{
+    return x-y;
+}
+
+var mul=(x,y)=>{
+    return x*y;
+}
+
 //function call
 var summation=sum(10,20);
 document.getElementById('demo').innerText="The summation is: "+summation;
+
+var subtraction=sub(50,10);
+document.getElementById('demo').innerText="The subtraction is: "+subtraction;
+
+var multiplication=mul(10,10);
+document.getElementById('demo').innerText="The multiplication is: "+multiplication;
