ec82941ca5df10ac80064276d2dcaf23bc63d85c
mamunurrashid1010 <96053862+mamunurrashid1010@users.noreply.github.com>
2022-07-24 09:56:50 +0600

    arrow function, how to use in a program example

---
+++ arrowFunction.js
@@ -0,0 +1,13 @@
+// Javascript ES6
+// Arrow function example
+// Arrow function: Arrow functions allows a short syntax for writing function expressions.
+// You don't need the function keyword, the return keyword, and the curly brackets.
+
+//arrow function
+const sum=(x,y)=>{
+    return x+y;
+}
+
+//function call
+var summation=sum(10,20);
+document.getElementById('demo').innerText="The summation is: "+summation;
