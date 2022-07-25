1d49debdd199d1a8a08f235300c2ac50b6bd2c54
mamunurrashid1010 <96053862+mamunurrashid1010@users.noreply.github.com>
2022-07-25 10:20:28 +0600

    for/of loop example, using string

--- for_of_loop_example.js
+++ for_of_loop_example.js
@@ -4,13 +4,22 @@
 
 document.write("For/of loop example:<br>");
 
+//  array example-1
 var data=["kamal","jamal","noman","niloy"];
 for (let x of data){
     document.write(x+"<br>");
 }
 
+// array example-2
 document.write("<br>Example-2:<br>");
 var country=["Bangladesh","India","Nepal","Pakistan"];
 for(let name of country){
     document.write(name+"<br>");
+}
+
+// string example-3
+document.write("<br>Example-3:<br>");
+var myString="Hello";
+for (let x of myString){
+    document.write(x+"<br>");
 }
