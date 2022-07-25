56350578e334bc8f19f746c49db5b4aaf15cfcd5
mamunurrashid1010 <96053862+mamunurrashid1010@users.noreply.github.com>
2022-07-25 10:08:55 +0600

    for/of loop example using array

---
+++ for_of_loop_example.js
@@ -0,0 +1,16 @@
+// Javascript ES6
+// for/of loop example
+
+
+document.write("For/of loop example:<br>");
+
+var data=["kamal","jamal","noman","niloy"];
+for (let x of data){
+    document.write(x+"<br>");
+}
+
+document.write("<br>Example-2:<br>");
+var country=["Bangladesh","India","Nepal","Pakistan"];
+for(let name of country){
+    document.write(name+"<br>");
+}
