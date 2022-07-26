a82d7871f7c27458e4fc0586c98e423a8f6f7952
mamunurrashid1010 <96053862+mamunurrashid1010@users.noreply.github.com>
2022-07-26 10:36:01 +0600

    map example, get value, map size, delete map element,  remove all data , key exist

--- map.js
+++ map.js
@@ -9,3 +9,26 @@ fruits.set("apple",200);
 fruits.set("orange",180);
 fruits.set("banana",100);
 fruits.set("mango",80);
+
+// Map.get(), the get() method gets the value of a key in a Map
+var orangePrice=fruits.get("orange");
+
+// Map.size, the size property returns the number of elements in a Map
+fruits.size;
+
+// Map.delete(), The delete() method removes a Map element.
+fruits.delete("apple");
+
+// Map.clear(), clear() method removes all the elements from a Map
+fruits.clear();
+
+// Map.has(), The has() method returns true if a key exists in a Map
+fruits.has("apple");
+
+// Maps are Objects, typeof returns object
+typeof fruits;
+
+// instanceof Map returns true
+fruits instanceof Map;
+
+document.getElementById('demo').innerText="Orange price: "+orangePrice;
