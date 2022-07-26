97374cb8832c2ec451e92cc87b7285b67382826d
mamunurrashid1010 <96053862+mamunurrashid1010@users.noreply.github.com>
2022-07-26 10:21:43 +0600

    map example, create map object and set value

---
+++ map.js
@@ -0,0 +1,11 @@
+// Javascript ES6
+// Maps example
+
+// Creates a new Map object
+const fruits=new Map();
+
+// set map value
+fruits.set("apple",200);
+fruits.set("orange",180);
+fruits.set("banana",100);
+fruits.set("mango",80);
