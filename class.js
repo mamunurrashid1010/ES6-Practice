3b881a51db15d7a3cb8f39a405f5b40510f37f20
mamunurrashid1010 <96053862+mamunurrashid1010@users.noreply.github.com>
2022-07-27 10:18:32 +0600

    function in a class example

--- class.js
+++ class.js
@@ -7,10 +7,19 @@ class car {
         this.name=name;
         this.model=model;
     }
-}
+
+    sum(a,b){
+        return a+b;
+    }
+    sub(a,b){
+        return a-b;
+    }
+};
 
 // object
 const obj=new car("BMW","M154");
 
 document.write("<br> Javascript ES6 class example:<br>");
 document.write("Name: "+obj.name+" Model: "+obj.model);
+document.write(" <br>The sum is: "+obj.sum(10,30));
+document.write(" <br>The sub is: "+obj.sub(30,10));
