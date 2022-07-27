8b986278ba457239a628d3af609240aee759e17c
mamunurrashid1010 <96053862+mamunurrashid1010@users.noreply.github.com>
2022-07-27 09:57:33 +0600

    create class.js and add example of class and constructor

---
+++ class.js
@@ -0,0 +1,16 @@
+// Javascript ES6
+// class example
+
+// class
+class car {
+    constructor(name,model){
+        this.name=name;
+        this.model=model;
+    }
+}
+
+// object
+const obj=new car("BMW","M154");
+
+document.write("<br> Javascript ES6 class example:<br>");
+document.write("Name: "+obj.name+" Model: "+obj.model);
