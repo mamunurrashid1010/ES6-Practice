02b03fc4b476ff0230134e1a49ed4a7577de0d62
mamunurrashid1010 <96053862+mamunurrashid1010@users.noreply.github.com>
2022-07-28 10:51:44 +0600

    string includes function example

---
+++ string.js
@@ -0,0 +1,14 @@
+// Javascript ES6
+// String operation
+
+
+// String.includes() function
+// The includes() method returns true if a string contains a specified value, otherwise false
+let text = "Hello world, welcome to the universe.";
+let result=text.includes("welcome");
+if(result==true){
+    document.write("<br>Word includes");
+}
+else {
+    document.write("<br>not includes");
+}
