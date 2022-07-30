fc8022ddb42d22b5d50aae1c1937d9a428476663
mamunurrashid1010 <96053862+mamunurrashid1010@users.noreply.github.com>
2022-07-30 10:18:13 +0600

    array convert from any object using Array.from() function

---
+++ array.js
@@ -0,0 +1,7 @@
+// javascript ES6
+
+// Array.from()
+// The Array.from() method returns an Array object from any object with a length property or any iterable object.
+var string="ABCDEF";
+var arr=Array.from(string); // [A,B,C,D,E,F]
+document.write("<br>"+arr);
