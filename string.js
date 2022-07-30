d73338684a0c968b778e94941ab80eaf90b3f2e5
mamunurrashid1010 <96053862+mamunurrashid1010@users.noreply.github.com>
2022-07-30 09:57:44 +0600

    string start with specific word, string operation startsWith() example

--- string.js
+++ string.js
@@ -2,7 +2,7 @@
 // String operation
 
 
-// String.includes() function
+// 1. String.includes() function
 // The includes() method returns true if a string contains a specified value, otherwise false
 let text = "Hello world, welcome to the universe.";
 let result=text.includes("welcome");
@@ -11,4 +11,16 @@ if(result==true){
 }
 else {
     document.write("<br>not includes");
+}
+
+
+// 2.String.startsWith()
+// The startsWith() method returns true if a string begins with a specified value, otherwise false
+let text1="Hello world, welcome to the universe.";
+let result1= text1.startsWith("Hello");
+if(result1==true){
+    document.write("<br>True");
+}
+else {
+    document.write("<br>False");
 }
