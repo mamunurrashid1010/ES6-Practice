f538495a7de79d8fa9b5413d1f34e9b2b28708f0
mamunurrashid1010 <96053862+mamunurrashid1010@users.noreply.github.com>
2022-07-31 09:56:59 +0600

    array find function example

--- array.js
+++ array.js
@@ -14,3 +14,12 @@ var keys=fruits.keys();
 for (let x of keys) {
     text += x + "<br>";
 }
+
+// Array find()
+// The find() method returns the value of the first array element that passes a test function.
+const numbers = [4, 9, 16, 25, 29];
+let first = numbers.find(myFunction);
+
+function myFunction(value, index, array) {
+    return value > 18;
+}
