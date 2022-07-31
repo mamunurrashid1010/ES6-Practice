7ade3a2827175b8125de926d3b4f05f9f6fa81d0
mamunurrashid1010 <96053862+mamunurrashid1010@users.noreply.github.com>
2022-07-31 09:53:50 +0600

    array key print using keys() function example

--- array.js
+++ array.js
@@ -4,4 +4,13 @@
 // The Array.from() method returns an Array object from any object with a length property or any iterable object.
 var string="ABCDEF";
 var arr=Array.from(string); // [A,B,C,D,E,F]
-document.write("<br>"+arr);
+document.write("<br>"+arr+"<br>");
+
+
+// Array keys()
+// The keys() method returns an Array Iterator object with the keys of an array.
+var fruits=["Orange","mango","Banana","Apple"];
+var keys=fruits.keys();
+for (let x of keys) {
+    text += x + "<br>";
+}
