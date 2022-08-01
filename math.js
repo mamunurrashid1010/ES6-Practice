fdaff34df547f315436614345cfd99b6dbc51540
mamunurrashid1010 <96053862+mamunurrashid1010@users.noreply.github.com>
2022-08-01 09:46:44 +0600

    Math.trunc() function example

---
+++ math.js
@@ -0,0 +1,9 @@
+// Javascript ES6
+// Math function
+
+// Math.trunc() function
+let v1= Math.trunc(10.7);
+    v1= Math.trunc(10.5);
+    v1= Math.trunc(10.3);
+    v1= Math.trunc(-10.2);
+document.write("<br> Value: "+v1);
