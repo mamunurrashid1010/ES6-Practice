50f9f43ff351d98d498f09cb9f76992e35b9cca2
mamunurrashid1010 <96053862+mamunurrashid1010@users.noreply.github.com>
2022-08-01 10:00:26 +0600

    Math.sign() and Math.cbrt() function example

--- math.js
+++ math.js
@@ -2,8 +2,21 @@
 // Math function
 
 // Math.trunc() function
-let v1= Math.trunc(10.7);
-    v1= Math.trunc(10.5);
-    v1= Math.trunc(10.3);
-    v1= Math.trunc(-10.2);
+let v1= Math.trunc(10.7); //return 10
+    v1= Math.trunc(10.5); //return 10
+    v1= Math.trunc(10.3); //return 10
+    v1= Math.trunc(-10.2);//return -10
 document.write("<br> Value: "+v1);
+
+// Math.sign() function
+// Math.sign(x) returns if x is negative, null or positive
+Math.sign(4); //return 1
+Math.sign(0); //return 0
+Math.sign(-4); //return -1
+
+// Math.cbrt() function
+// Math.cbrt(x) returns the cube root of x.
+Math.cbrt(8);    // returns 2
+Math.cbrt(64);    // returns 4
+Math.cbrt(125);    // returns 5
+document.write("<br> Value: "+Math.cbrt(8));
