b985bb4ed9650ae33d15746f19ea4e28c389792d
mamunurrashid1010 <96053862+mamunurrashid1010@users.noreply.github.com>
2022-08-02 09:49:34 +0600

    check given number is safe int or not using Number..isSafeInteger() function

--- number.js
+++ number.js
@@ -14,3 +14,16 @@ if (checkInt==true)
     document.write('<br>Integer Number');
 else
     document.write('Not Integer');
+
+
+
+// Number.isSafeInteger() Method
+// A safe integer is an integer that can be exactly represented as a double precision number.
+// The Number.isSafeInteger() method returns true if the argument is a safe integer.
+
+var safeInteger=Number.isSafeInteger(10); //return true
+    safeInteger=Number.isSafeInteger(12345678901234567890); //return false
+if (safeInteger==true)
+    document.write('<br>Safe Integer');
+else
+    document.write('Not Safe Integer');
